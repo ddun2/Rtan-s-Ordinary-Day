cc7384aa45a032a4d2cd66d76861a345e77527b7
Sungjun Park <ddun4917@gmail.com>
2024-05-09 16:43:28 +0900

    Update PlayerMovement

---
+++ Assets/Scripts/Behaviors/RODMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class RODMovement : MonoBehaviour
+{
+    private RODController movementController;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+
+    private void Awake()
+    {
+        movementController = GetComponent<RODController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        movementController.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        movementRigidbody.velocity = direction;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+}
---
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : RODController
+{
+    private Camera camera;
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+}
